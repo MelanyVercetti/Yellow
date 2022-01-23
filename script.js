c23d5cf2255b13172bbc83d42a61767476694c49
Melany <melany777@mail.ru>
2022-01-23 13:12:30 +0600

    восстановлена фоновая картинка на десктопе

--- script.js
+++ script.js
@@ -3,14 +3,12 @@ const menuBtn = document.querySelector('.menu');
 const menuClose = document.querySelector('.menu-close');
 
 menuBtn.addEventListener('click', () => {
-    console.log('menu click')
     navList.classList.toggle('active');
     menuBtn.classList.toggle('active');
     menuClose.classList.toggle('active');
 });
 
 menuClose.addEventListener('click', () => {
-    console.log('menu close')
     navList.classList.toggle('active');
     menuBtn.classList.toggle('active');
     menuClose.classList.toggle('active');
